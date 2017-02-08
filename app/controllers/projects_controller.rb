e9dcba0ec6959a0c6783a2276af9ed8c566935ef
samgrimm <samantha.grimm@gmail.com>
2017-02-08 11:14:19 -0600

    integreated new and create actions to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,28 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to projects_path, notice: 'Project is now live.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :subtitle, :body,:main_image, :thumb_image )
+  end
+
 end
