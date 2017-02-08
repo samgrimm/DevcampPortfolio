03a53eb9b4b0e3427d2daca22d6a22e576be02b2
samgrimm <samantha.grimm@gmail.com>
2017-02-08 11:22:38 -0600

    added the edit, update, and destroy actions with links to the project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:edit, :update, :destroy]
   def index
     @projects = Project.all
   end
@@ -20,10 +21,38 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to projects_path, notice: 'Project has been updated.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: 'Project was successfully removed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def project_params
     params.require(:project).permit(:title, :subtitle, :body,:main_image, :thumb_image )
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 
 end
