ac24c9bd288bf9207bcc509fdff241ad8d48db78
samgrimm <samantha.grimm@gmail.com>
2017-02-11 06:58:10 -0600

    updated ruby gems to add my own view tool gem

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~>4.2'
 
 gem 'bootstrap', '~>4.0.0.alpha6'
+
+gem 'sam_view_tool',git: 'https://github.com/samgrimm/sam-view-tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
        content_tag(:p, greeting, class: "source-greeting")
      end
   end
+
+
+  def copyright_generator
+    @copyright = SamViewTool::Renderer.copyright 'Samantha Cabral', 'All rights reserved'
+  end
 end
