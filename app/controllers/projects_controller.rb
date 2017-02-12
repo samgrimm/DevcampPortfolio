1c4b4a9becf0949745343037149552efa5a2d576
samgrimm <samantha.grimm@gmail.com>
2017-02-12 10:45:12 -0600

    implemented authorization rules for the drag and drop function

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:edit, :update, :destroy, :show]
   layout "project"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
 
   def index
     @projects = Project.by_position
