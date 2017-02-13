500606d0354082b12b8b01f48cf1068e7fd5e03c
samgrimm <samantha.grimm@gmail.com>
2017-02-13 06:31:55 -0600

    updated image upload forms and placeholder images

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,16 @@
 module ProjectsHelper
+  def image_generator(height:, width: )
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def project_img img, type
+    if img.model.main_image? || img.model.thumb_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    else
+      image_generator(height: '600', width: '400')
+    end
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,11 @@
 class Project < ApplicationRecord
   include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
   mount_uploader :thumb_image, ProjectUploader
-  mount_uploader :main_image, ProjectUploader                              
+  mount_uploader :main_image, ProjectUploader
 
   def self.angular
     where(subtitle: "Angular")
@@ -17,10 +17,5 @@ class Project < ApplicationRecord
 
   scope :ruby_on_rails_projects, -> { where(subtitle: "Ruby on Rails") }
 
-  after_initialize :set_defaults
 
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 end
