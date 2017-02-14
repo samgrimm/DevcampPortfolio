3292b08301931b0be209cec2138889e65f089cfa
samgrimm <samantha.grimm@gmail.com>
2017-02-14 13:43:37 -0600

    built out topic views

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,4 +3,8 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
   end
+
+  def topics_widget
+    link_to "Topics", topics_path, class:"badge badge-pill badge-default"
+  end
 end
