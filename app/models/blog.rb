aa9adccbe05e187e2f3ff68cbb73c68db5de4fce
samgrimm <samantha.grimm@gmail.com>
2017-02-13 19:35:38 -0600

    added comment resources and redis db

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,6 +3,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   belongs_to :topic
+  has_many :comments, dependent: :destroy
 
   validates_presence_of :title, :body
 
