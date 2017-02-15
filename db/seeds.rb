fb4ed116230945f8484531ec1de2b74c79760eda
samgrimm <samantha.grimm@gmail.com>
2017-02-15 11:24:50 -0600

    added users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+User.create!(
+  email: "test@test.com",
+  password:"foobar",
+  password_confirmation: "foobar",
+  name: "Admin User",
+  roles: "site_admin"
+)
+puts "1 admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password:"foobar",
+  password_confirmation: "foobar",
+  name: "Regular User"
+)
+puts "1 regular user created"
+
 3.times do |topic|
   Topic.create!(
   title: "Topic #{topic}"
