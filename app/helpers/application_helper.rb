6cc27b64518ecf2b10319c55a3b0ceaefac1e715
samgrimm <samantha.grimm@gmail.com>
2017-03-01 19:18:04 -0600

    moved markdown helper to the application so that it will work for both the blog and the project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -86,4 +86,25 @@ module ApplicationHelper
   def alert_generator msg
     js add_gritter(msg, title: "Samantha Cabral Portfolio", sticky: false)
   end
+
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def markdown(text)
+    language ||= :plaintext
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true,
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
